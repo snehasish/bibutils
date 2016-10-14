c526c0a9da3d39b86e5d0049dded8979b144a296
Snehasish Kumar <ska124@sfu.ca>
2016-10-13 20:02:03 -0700

    Updated formatting

--- bibutil/html.py
+++ bibutil/html.py
@@ -1,3 +1,10 @@
 def main(bibs):
     for p in sorted(bibs.entries, key = lambda x : int(x['year']), reverse=True):
-        print p['title'].translate({ord(c): None for c in '{}'})
+        print '<div class="col-md-12"><p class="pub-p">'
+        print '<span class="pub-title">', \
+                p['title'].translate({ord(c): None for c in '{}'}),'</span> <br/>'
+        print '<span class="pub-author">', p['author'], '</span> <br/>'
+        if 'booktitle' in p.keys(): 
+            print '<span class="pub-booktitle">', p['booktitle'], '</span> <br/>'
+        print '<span class="pub-year">', p['year'], '</span>'
+        print '</p></div>'
