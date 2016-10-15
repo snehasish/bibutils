90a947f437c96889b7abcf172524d2e47b682049
Snehasish Kumar <ska124@sfu.ca>
2016-10-15 15:52:40 -0700

    Updated HTML generation

--- bibutil/html.py
+++ bibutil/html.py
@@ -3,8 +3,23 @@ def main(bibs):
         print '<div class="col-md-12"><p class="pub-p">'
         print '<span class="pub-title">', \
                 p['title'].translate({ord(c): None for c in '{}'}),'</span> <br/>'
-        print '<span class="pub-author">', p['author'], '</span> <br/>'
+        # s=','.join([ ' '.join(s.split(',').reverse()) for s in p['author'].split('and')])
+        auth_str = ''
+        auth_list = [list(reversed(s.split(','))) for s in p['author'].split(' and ')] 
+        flag = False
+        for a in auth_list:
+            if flag:
+                auth_str += ', '
+            s = ' '.join([x.strip() for x in a])
+            if s == 'Snehasish Kumar':
+                s = '<span class="myname">Snehasish Kumar</span>'
+            auth_str += s 
+            flag = True
+        print '<span class="pub-author">', auth_str, '</span> <br/>'
         if 'booktitle' in p.keys(): 
             print '<span class="pub-booktitle">', p['booktitle'], '</span> <br/>'
         print '<span class="pub-year">', p['year'], '</span>'
+        # print p.keys()
+        if 'link' in p.keys():
+            print '<span class="pub-url"><a  target="_blank" href="', p['link'], '">[url]</a></span>'
         print '</p></div>'
